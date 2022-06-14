1a0b572d26defa81a9b4f886145a82f4da80d017
tsishchanka <e.tsish@gmail.com>
2022-06-15 00:21:13 +0300

    init todoList

---
+++ src/components/App/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import TodoItem from './components/TodoItem';
+
+function App() {
+  return (
+    <div className='App'>
+      <TodoItem />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/TodoItem/styled.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  max-width: 1000px;
+  padding: 0 15px;
+  margin: 0 auto;
+`;
+
+export const Title = styled.h1`
+  color: #16527a;
+  text-align: center;
+`;
+
+export const TodoItemWrapper = styled.div`
+  display: flex;
+`;
+
+export const ButtonsWrapper = styled.div`
+  padding: 10px;
+`;
+
+export const ErrorMessageField = styled.div`
+  color: green;
+  padding: 5px 15px;
+  margin-bottom: 15px;
+`;
